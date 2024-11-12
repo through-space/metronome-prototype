a84ed8eb72c24fb6d438e4eef8f0a132951e6ab4
lvil <mysecretemail@gmail.com>
2024-11-12 13:22:55 +0200

    Added 7segmentDisplay. Added prettier

---
+++ src/components/molecules/SegmentsDisplay/SegmentsDisplay.tsx
@@ -0,0 +1,17 @@
+import { Display } from "7-segment-display/src";
+import charToDigit from "7-segment-display/src/utils/charToDigit";
+import { ISegmentsDisplayProps } from "@components/molecules/SegmentsDisplay/SegmentsDisplayInterfaces";
+
+export const SegmentsDisplay = (props: ISegmentsDisplayProps) => {
+	const { value } = props;
+	const charMap = {
+		...charToDigit,
+		_: [0, 0, 0, 1, 0, 0, 0],
+		" ": [0, 0, 0, 0, 0, 0, 0],
+	};
+	return (
+		<>
+			<Display count={4} value={value} charMap={charMap} />
+		</>
+	);
+};
